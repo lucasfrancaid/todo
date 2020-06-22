3add5549bc6a2c44c728a45e6e3c1d93f404ed3e
lucasfrancaid <lucasfrancaid@gmail.com>
2020-06-22 00:45:19 -0300

    Adjusted layout and styles.

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,178 +1,192 @@
 <template>
-    <v-container style="max-width: 500px; margin-top: 2.5em">
-        <v-text-field
-          v-model="task"
-          label="What are you working on?"
-          solo
-          @keydown.enter="createTask"
-        >
-            <v-fade-transition v-slot:append>
-                <v-icon
-                    v-if="task"
-                    @click="createTask"
-                >
-                    add_circle
-                </v-icon>
-            </v-fade-transition>
-        </v-text-field>
+	<v-container style="max-width: 500px; margin-top: 1em">
+		<v-text-field
+			v-model="task"
+			label="What are you working on?"
+			solo
+			@keydown.enter="createTask"
+		>
+			<v-fade-transition v-slot:append>
+				<v-icon
+					v-if="task"
+					@click="createTask"
+				>
+					add_circle
+				</v-icon>
+			</v-fade-transition>
+		</v-text-field>
 
-        <h2 class="display-1 success--text pl-4">
-            Tasks:&nbsp;
-            <v-fade-transition leave-absolute>
-                <span :key="`tasks-${tasks.length}`">
-                    {{ tasks.length }}
-                </span>
-            </v-fade-transition>
-        </h2>
+		<!--<h2 class="display-1 pl-1">
+			Tasks:&nbsp;
+			<v-fade-transition leave-absolute>
+				<span :key="`tasks-${tasks.length}`">
+					{{ tasks.length }}
+				</span>
+			</v-fade-transition>
+		</h2>-->
 
-        <v-divider class="mt-4"></v-divider>
+		<v-divider class="mt-0"></v-divider>
 
-        <v-row
-          class="my-1"
-          align="center"
-        >
-            <strong class="mx-4 info--text text--darken-2">
-            Remaining: {{ remainingTasks }}
-            </strong>
+		<v-row
+			class="my-1"
+			align="center"
+		>
 
-            <v-divider vertical></v-divider>
+			<strong class="ml-16 mr-8 text--darken-2">
+			TASKS:
+			<v-fade-transition leave-absolute>
+				<span :key="`tasks-${tasks.length}`">
+					{{ tasks.length }}
+				</span>
+			</v-fade-transition>
+			</strong>
 
-            <strong class="mx-4 success--text text--darken-2">
-            Completed: {{ completedTasks }}
-            </strong>
+			<v-divider vertical></v-divider>
 
-            <v-spacer></v-spacer>
+			<strong class="mx-12 text--darken-2">
+			TO DO: {{ remainingTasks }}
+			</strong>
 
-            <v-progress-circular
-            :value="progress"
-            class="mr-2"
-            ></v-progress-circular>
-        </v-row>
+			<v-divider vertical></v-divider>
 
-        <v-divider class="mb-4"></v-divider>
+			<strong class="ml-10 mr-2 text--darken-2">
+			DONE: {{ completedTasks }}
+			</strong>
 
-            <v-card v-if="tasks.length > 0">
-                <v-slide-y-transition
-                class="py-0"
-                group
-                name="v-list"
-                >
-                    <template v-for="(task, i) in tasks">
-                        <v-divider
-                        v-if="i !== 0"
-                        :key="`${i}-divider`"
-                        ></v-divider>
+			<v-spacer></v-spacer>
 
-                        <v-list-item :key="`${i}-${task.id}`">
-                            <v-list-item-action>
-                                <v-checkbox
-                                @change="updateTask(task)"
-                                v-model="task.done"
-                                :color="task.done && 'grey' || 'primary'"
-                                >
-                                <template v-slot:label>
-                                    <div
-                                    :class="task.done && 'grey--text' || 'primary--text'"
-                                    class="ml-4"
-                                    v-text="task.title"
-                                    ></div>
-                                </template>
-                                </v-checkbox>
-                            </v-list-item-action>
+			<v-progress-circular
+			:value="progress"
+			color="success"
+			class="mr-4"
+			></v-progress-circular>
+		</v-row>
 
-                            <v-scroll-x-transition>
-                                <v-icon
-                                v-if="task.done"
-                                color="success"
-                                >
-                                done
-                                </v-icon>
-                            </v-scroll-x-transition>
+		<v-divider class="mb-5"></v-divider>
 
-                            <v-spacer></v-spacer>
+			<v-card v-if="tasks.length > 0">
+				<v-slide-y-transition
+				class="py-0"
+				group
+				name="v-list"
+				>
+					<template v-for="(task, i) in tasks">
+						<v-divider
+						v-if="i !== 0"
+						:key="`${i}-divider`"
+						></v-divider>
 
-                            <v-icon
-                            :key="`${task.id}-delete`"
-                            @click="removeTask(task.id)"
-                            >
-                            delete
-                            </v-icon>
-                        </v-list-item>
-                    </template>
-                </v-slide-y-transition>
-            </v-card>
-        </v-container>
+						<v-list-item :key="`${i}-${task.id}`">
+							<v-list-item-action>
+								<v-checkbox
+								@change="updateTask(task)"
+								v-model="task.done"
+								:color="task.done && 'grey' || 'success'"
+								>
+								<template v-slot:label>
+									<div
+									:class="task.done && 'grey--text' || 'success--text'"
+									class="ml-4"
+									v-text="task.title"
+									></div>
+								</template>
+								</v-checkbox>
+							</v-list-item-action>
+
+							<v-scroll-x-transition>
+								<v-icon
+								v-if="task.done"
+								color="success"
+								class="mr-8"
+								>
+								done
+								</v-icon>
+							</v-scroll-x-transition>
+
+							<v-spacer></v-spacer>
+
+							<v-icon
+							:key="`${task.id}-delete`"
+							@click="removeTask(task.id)"
+							>
+							delete
+							</v-icon>
+						</v-list-item>
+					</template>
+				</v-slide-y-transition>
+			</v-card>
+		</v-container>
 </template>
 
 <script>
-    import api from '../services/api';
+	import api from '../services/api';
 
-    export default {
+	export default {
 
-        data: () => ({
-            tasks: [],
-            task: null,
-        }),
-        created() {
-            api.get('api/task')
-                .then((res) => res.data.map(task => this.tasks.push(task)))
-                .catch(err => console.log(err.data)
-            );
-        },
+		data: () => ({
+			tasks: [],
+			task: null,
+		}),
+		created() {
+			api.get('api/task')
+				.then((res) => res.data.map(task => this.tasks.push(task)))
+				.catch(err => console.log(err.data)
+			);
+		},
 
-        computed: {
-            completedTasks () {
-                return this.tasks.filter(task => task.done).length
-            },
-            progress () {
-                return this.completedTasks / this.tasks.length * 100
-            },
-            remainingTasks () {
-                return this.tasks.length - this.completedTasks
-            },
-        },
+		computed: {
+			completedTasks () {
+				return this.tasks.filter(task => task.done).length
+			},
+			progress () {
+				return this.completedTasks / this.tasks.length * 100
+			},
+			remainingTasks () {
+				return this.tasks.length - this.completedTasks
+			},
+		},
 
-        methods: {
-            createTask () {
-                const data = {
-                    done: false,
-                    title: this.task,
-                    description: '',
-                }
+		methods: {
+			createTask () {
+				const data = {
+					done: false,
+					title: this.task,
+					description: '',
+				}
 
-                api.post('api/task', data)
-                    .then((res) => {
-                        const data = res.data;
-                        console.log('data post', data)
-                        this.tasks.push({
-                            id: data.id,
-                            title: this.task,
-                            description: data.description,
-                        })
-                        this.task = null
-                    })
-                    .catch(err => console.log(err.data)
-                );
-            },
-            updateTask (task) {
-                api.put(`api/task/${task.id}`, task)
-                    .then((res) => {
-                        const itemIndex = this.tasks.findIndex(element => element.id === res.data.id)
-                        this.tasks[itemIndex] = task
-                    })
-                    .catch(err => console.log(err.data)
-                );
-            },
-            removeTask (id) {
-                api.delete(`api/task/${id}`)
-                    .then((res) => {
-                        console.log('Task was deleted', res.data)
-                        const itemIndex = this.tasks.findIndex(element => element.id === id)
-                        this.tasks.splice(itemIndex, 1)
-                    })
-                    .catch(err => console.log(err.data)
-                );
-            }
-        },
-    }  
+				api.post('api/task', data)
+					.then((res) => {
+						const data = res.data;
+						console.log('data post', data)
+						this.tasks.push({
+							id: data.id,
+							title: this.task,
+							description: data.description,
+						})
+						this.task = null
+					})
+					.catch(err => console.log(err.data)
+				);
+			},
+			updateTask (task) {
+				api.put(`api/task/${task.id}`, task)
+					.then((res) => {
+						const itemIndex = this.tasks.findIndex(element => element.id === res.data.id)
+						this.tasks[itemIndex] = task
+					})
+					.catch(err => console.log(err.data)
+				);
+			},
+			removeTask (id) {
+				api.delete(`api/task/${id}`)
+					.then((res) => {
+						console.log('Task was deleted', res.data)
+						const itemIndex = this.tasks.findIndex(element => element.id === id)
+						this.tasks.splice(itemIndex, 1)
+					})
+					.catch(err => console.log(err.data)
+				);
+			}
+		},
+	}  
 </script>
